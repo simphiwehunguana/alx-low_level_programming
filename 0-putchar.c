ee71a1a2a3f1b8bc3f0e19003d34e991dd93de82
simphiwe <simphiwehunguane@gmail.com>
2022-12-13 04:41:50 -0800

    Task 0

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return 0
+ */
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_puchar('u');
+	_puchar('t');
+	_puchar('c');
+	_puchar('h');
+	_puchar('a');
+	_puchar('r');
+	_putchar('\n');
+
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-low_level_programming
-I'm about to code using C programme
+already startde with nested loops
